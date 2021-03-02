629297b76b30ad6c9fc6ea7b0bc8214d151e1b1b
shadow046 <emorej046@gmail.com>
2021-03-02 11:18:48 +0800

    Hot fixed

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,7 +66,6 @@ class Kernel extends HttpKernel
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'role_or_permissions' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
         'ajax'=>\App\Http\Middleware\AjaxRequestOnly::class,
-        'ajax-session-expired' => \App\Http\Middleware\AjaxSessionExpiredMiddleware::class,
     ];
 
     /**
--- public/js/app.js
+++ public/js/app.js
@@ -65906,7 +65906,3 @@ module.exports = __webpack_require__(/*! /var/www/laravel/resources/sass/app.scs
 /***/ })
 
 /******/ });
-$( document ).ajaxError(function( event, jqxhr, settings, thrownError ) {
-  alert("Session expired. You'll be take to the login page");
-  location.href = "/login"; 
-});
