9bb2c0b4c3c726462b6b6fc4429b32539eb5a449
shadow046 <emorej046@gmail.com>
2021-03-23 16:37:15 +0800

    Fixed

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::any('myapi', 'HomeController@myapi');
+Route::get('activity', 'HomeController@activity');//->middleware('ajax');
 
