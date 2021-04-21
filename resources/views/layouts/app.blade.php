64a2edeb4159a5081638dd71cdf62bb91f11bf9a
shadow046 <emorej046@gmail.com>
2021-04-21 08:59:38 +0800

    Update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @auth
+        @if (Session::has('success') && Request::is('report-a-problem'))
+            <meta http-equiv="refresh" content="5;url={{ url('/') }}">
+        @else
             @if(auth()->user()->branch->branch == "Warehouse")
                 <meta http-equiv="refresh" content="800">
             @else
@@ -23,6 +26,7 @@
                 @endif
             @endif
             <meta name="ctok" content="{{ csrf_token() }}">
+        @endif
         @endauth
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="{{ url('/css/style.css') }}" />
