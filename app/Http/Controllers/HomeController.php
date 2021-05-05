7b4612a853a3fd8974ab47357dc1485a4810ac1f
shadow046 <emorej046@gmail.com>
2021-05-05 15:38:53 +0800

    Fixed tech logs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -432,7 +432,7 @@ class HomeController extends Controller
                     'branch',
                 )
                 ->join('users', 'users.id', 'user_id')
-                ->join('branches', 'branches.id', 'branch_id');
+                ->join('branches', 'branches.id', 'user_logs.branch_id');
         }
         return DataTables::of($act)
         ->addColumn('id', function (UserLog $request){
