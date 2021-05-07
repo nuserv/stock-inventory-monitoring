a14cf06d41c87403c7ab7f56be2451fdff1944b4
shadow046 <emorej046@gmail.com>
2021-05-07 17:16:50 +0800

    Update

--- app/Http/Controllers/DefectiveController.php
+++ app/Http/Controllers/DefectiveController.php
@@ -66,15 +66,15 @@ class DefectiveController extends Controller
             ->where('branch_id', auth()->user()->branch->id)
             ->where('status', 'For return')
             ->join('items', 'defectives.items_id', '=', 'items.id')
-            ->wherein('defectives.status', ['For return', 'For receiving']);
+            ->wherein('defectives.status', ['For return', 'For receiving'])->get();
         $waredef =Defective::query()->select('branches.branch', 'defectives.category_id', 'branches.id as branchid', 'defectives.updated_at', 'defectives.id as id', 'items.item', 'items.id as itemid', 'defectives.serial', 'defectives.status')
             ->where('defectives.status', 'Repaired')
             ->join('items', 'defectives.items_id', '=', 'items.id')
-            ->join('branches', 'defectives.branch_id', '=', 'branches.id');
+            ->join('branches', 'defectives.branch_id', '=', 'branches.id')->get();
         $main =Defective::query()->select('branches.branch', 'defectives.category_id', 'branches.id as branchid', 'defectives.updated_at', 'defectives.id as id', 'items.item', 'items.id as itemid', 'defectives.serial', 'defectives.status')
             ->wherein('defectives.status', ['Repaired', 'For Repair'])
             ->join('items', 'defectives.items_id', '=', 'items.id')
-            ->join('branches', 'defectives.branch_id', '=', 'branches.id');
+            ->join('branches', 'defectives.branch_id', '=', 'branches.id')->get();
         $repair = Defective::query()->select('branches.branch', 'defectives.category_id', 'branches.id as branchid', 'defectives.updated_at', 'defectives.id as id', 'items.item', 'items.id as itemid', 'defectives.serial', 'defectives.status')
             ->wherein('defectives.status', ['For receiving', 'For repair', 'Repaired'])
             ->join('items', 'defectives.items_id', '=', 'items.id')
@@ -96,7 +96,6 @@ class DefectiveController extends Controller
             $cat = Category::where('id', $data->category_id)->first();
             return $cat->category;
         })
-        
         ->addColumn('status', function (Defective $data){
             return $data->status;
         })
