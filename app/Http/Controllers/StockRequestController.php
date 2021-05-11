78d6722d7044795877564983cb068f2dbe3c35af
shadow046 <emorej046@gmail.com>
2021-05-11 15:25:52 +0800

    Hot Fixed

--- app/Http/Controllers/StockRequestController.php
+++ app/Http/Controllers/StockRequestController.php
@@ -867,14 +867,14 @@ class StockRequestController extends Controller
                 $log = new UserLog;
                 $log->branch_id = auth()->user()->branch->id;
                 $log->branch = auth()->user()->branch->branch;
-                $log->activity = "UPDATE SCHEDULED delivery for $branch with Request no. $request->reqno ";
+                $log->activity = "UPDATE SCHEDULED DELIVERY for $branch with Request no. $request->reqno ";
                 $log->user_id = auth()->user()->id;
                 $log->fullname = auth()->user()->name.' '.auth()->user()->middlename.' '.auth()->user()->lastname;
             }else{
                 $log = new UserLog;
                 $log->branch_id = auth()->user()->branch->id;
                 $log->branch = auth()->user()->branch->branch;
-                $log->activity = "SCHEDULED delivery for $branch with Request no. $request->reqno ";
+                $log->activity = "SCHEDULED DELIVERY for $branch with Request no. $request->reqno ";
                 $log->user_id = auth()->user()->id;
                 $log->fullname = auth()->user()->name.' '.auth()->user()->middlename.' '.auth()->user()->lastname;
             }
