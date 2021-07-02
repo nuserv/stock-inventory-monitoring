52775135482a42980b1f70f7eb4b1cee9a165ed2
shadow046 <emorej046@gmail.com>
2021-07-02 13:00:16 +0800

    Hot fixed

--- public/js/branch/billable.js
+++ public/js/branch/billable.js
@@ -29,7 +29,7 @@ $(document).ready(function()
 });
 
 $(document).on('click', '#reqBtn', function(){
-    $('#service-inModal').modal({backdrop: 'static', keyboard: false}); 
+    $('#service-unitModal').modal({backdrop: 'static', keyboard: false}); 
 });
 
 $(document).on('click', '#delBtn', function(e){
