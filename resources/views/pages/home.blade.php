281eb2887088ac0e1d1dcfd32a4df0fdcfb0b6d1
shadow046 <emorej046@gmail.com>
2021-08-13 11:28:46 +0800

    Hot fixed

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -32,7 +32,7 @@
                                 @endif
                                 @if (!auth()->user()->hasanyrole('Manager', 'Editor'))
                                 <div class="col-sm-2">
-                                    <a href="{{ route('repaired.list')}}">
+                                    <a href=@if (auth()->user()->hasanyrole("Warehouse Manager", "Encoder")) {{ route('repaired.list')}} @else {{ route('return.index')}} @endif>
                                         <div class="card bg-card">
                                             <div class="card-body text-center">
                                                 <p class="card-text" style="font-size: 12px">@if (auth()->user()->hasanyrole('Warehouse Manager', 'Encoder')) REPAIRED @else RETURNS @endif</p>
