dbd05a1c4572d88cb16229bba103714e8b9be4ee
shadow046 <emorej406@gmail.com>
2022-10-24 12:49:37 +0800

    Update

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -50,9 +50,9 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            <div class="code">
+            <div>
                 {{-- @yield('code') --}}
-                Page Not Found... <br>
+                Page Not Found... <br><hr>
             </div>
 
             <div class="message" style="padding: 10px;">
