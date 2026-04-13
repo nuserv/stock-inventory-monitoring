5ccebc40c4116814090c764e65c9c36491f16624
shadow046 <emorej046@gmail.com>
2026-04-13 08:59:26 +0800

    Refactor 503 error page to extend minimal layout and update title and message

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,3 +1,7 @@
-@section('title', __('Service Unavailable'))
-@section('code', '503')
+@extends('errors::minimal')
+
+@section('title', __('Page Not Found'))
+@section('code', 'Page Not Found')
 @section('message', __('We are currently under maintenance. Please check back later.'))
+
+<!-- @section('message', __($exception->getMessage() ?: 'The page you are trying to access has been moved. Please click this <a href="https://stockmonitoring.apsoft.com.ph">link</a> to proceed.')) -->
